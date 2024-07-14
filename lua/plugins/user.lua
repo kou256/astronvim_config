146ad1b1eed969e51bb71f0ae7549dd83ce1a550
kou256 <25428510+kou256@users.noreply.github.com>
2024-07-14 22:43:42 +0900

    💄 style(ui): カラースキームを変更

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
@@ -82,4 +82,12 @@ return {
       )
     end,
   },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup {
+        filter = "spectrum"
+      }
+    end
+  }
 }
